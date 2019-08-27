64e654dc9fb65b6f9a12c4f8a532677fc8b7980c
soroush <soroush@tails.com>
2019-08-27 21:43:20 +0100

    not needed deleted.

--- code/flask_blog.py
+++ code/flask_blog.py
@@ -3,11 +3,17 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def hello_world():
-    return '<h1>Hello, World!</h1>'
+    return '<h1>Home Page</h1>'
+
+
+@app.route('/about')
+def about():
+    return '<h1>About</h1>'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
 
