4ed5169b1d20bb05c556696aa57fd408c64225d6
soroush <soroush@tails.com>
2019-09-15 18:57:18 +0100

    Section 3 is finished.

--- code/flask_blog.py
+++ code/flask_blog.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 app = Flask(__name__)
+app.config["SECRET_KEY"]= "soroush"
 
 posts = [
     {
@@ -16,6 +17,33 @@ posts = [
     }
 ]
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("username",
+                           validators=[DataRequired(),
+                                       Length(min=2, max=20)])
+    email = StringField("Email", validators=[DataRequired(),
+                                             Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(),
+                                                 EqualTo('password')])
+    submit = SubmitField("Sign Up")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember me")
+    submit = SubmitField("Log in")
+
+
+
+
 
 @app.route("/")
 @app.route("/home")
@@ -28,7 +56,33 @@ def about():
     return render_template('about.html', title='About')
 
 
+@app.route('/register', methods=["POST", "GET"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for("home"))
+    return render_template('register.html', title="register", form=form)
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "a@a.com" and form.password.data == 'password':
+            flash('you have been logged in', 'success')
+            return redirect(url_for("home"))
+        else:
+            flash("Login unsuceesful", 'danger')
+
+
+
+    return render_template('login.html', title="login", form=form)
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
