b22f825441486634fcd52c58baac41f85b76853e
soroush <soroush@tails.com>
2019-09-27 12:04:02 +0100

    The models are recreated and flask_sqlalchemy is remembered again.

--- code/flask_blog.py
+++ code/flask_blog.py
@@ -3,29 +3,33 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "soroush"
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    image_file = db.Column(db.String(20), unique=True, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref="author", lazy=True)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User({self.username}, {self.email}, {self.password})"
+        return f"User('{self.username}','{self.email}')"
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow )
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}'"
+        return f"Post('{self.title}','{self.date_posted}')"
+
 
 
 
@@ -74,6 +78,7 @@ class LoginForm(FlaskForm):
 
 @app.route("/")
 @app.route("/home")
+
 def home():
     return render_template('home.html', posts=posts)
 
