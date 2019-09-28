6240412c1ec0a44c91123a4cbb151fe7463d226f
soroush <soroush@tails.com>
2019-09-28 22:01:53 +0100

    We can checkc if the username exists or not.

--- code/flask_blog/forms.py
+++ code/flask_blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_blog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField("username",
@@ -13,6 +14,16 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(),
                                                  EqualTo('password')])
     submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("The username is already taken, Please define another one.")
+
+    def validate_email(self, email):
+        user_by_email = User.query.filter_by(email=email.data).first()
+        if user_by_email:
+            raise ValidationError("The email is defined for another user. Choose a different one please.")
 
 
 class LoginForm(FlaskForm):
